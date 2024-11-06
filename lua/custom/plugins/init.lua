a4bc810db2759c1b430a756d5e01c13a23cf7929
Van Tran <tmvan.1903@gmail.com>
2024-11-07 02:13:54 +0700

    add my pde setups

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,49 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'ggandor/leap.nvim',
+    --version = '',
+    dependencies = {
+      'tpope/vim-repeat',
+    },
+    init = function()
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      map('n', 's', '<Plug>(leap)', { desc = 'Leap' })
+    end,
+  },
+  { 'mbbill/undotree' },
+  {
+    'ThePrimeagen/harpoon',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    init = function()
+      local harpoon = require 'harpoon.ui'
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      map('n', '<leader>he', function()
+        harpoon.toggle_quick_menu()
+      end, { desc = '[H]arpoon [E]ditor' })
+
+      map('n', '<leader>a', function()
+        harpoon.add_file()
+      end, { desc = '[A]dd to Harpoon' })
+
+      for i = 1, 9 do
+        map('n', '<leader>' .. i, function()
+          harpoon.nav_file(1)
+        end, { desc = 'Harpoon: Navigate to [' .. i .. ']' })
+      end
+    end,
+  },
+}
